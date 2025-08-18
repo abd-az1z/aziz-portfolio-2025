53edce431d53d3140271b055ac78ee275082efae
Abdulaziz <mohdabdulaziz2023@gmail.com>
2025-08-18 02:13:40 -0700

    04-PortfolioMosaic-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Header from "@/modules/home/Header";
 import HeroSection from "@/modules/home/HeroSection";
+import { PortfolioMosaic } from "@/modules/home/PortfolioMosaic";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div className="min-h-screen  w-full flex flex-col items-center bg-background px-2 sm:px-4 md:px-8 lg:px-16 transition-all duration-300">
-      <div className="w-full">
-        <Header />
-      </div>
-      <div>
+    <div className="min-h-screen w-full bg-background">
+      <Header />
+      <main className="pt-16">
         <HeroSection />
-      </div>
+        <PortfolioMosaic />
+      </main>
     </div>
   );
-};
-export default Home;
+}
