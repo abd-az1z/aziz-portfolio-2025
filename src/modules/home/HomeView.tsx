16d1bf18d753c8088f2c1d8a82299332cf0b6e97
Abdulaziz <mohdabdulaziz2023@gmail.com>
2025-08-22 23:46:08 -0700

    10-curated-work

--- src/modules/home/HomeView.tsx
+++ src/modules/home/HomeView.tsx
@@ -2,16 +2,17 @@ import { CuratedWork } from "./ui/CuratedWork";
 import Header from "./ui/Header";
 import HeroSection from "./ui/HeroSection";
 import PortfolioMosaic from "./ui/PortfolioMosaic";
+import TechStackSection from "./ui/TechStackSection";
 
 const HomeView = () => {
   return (
     <div className="min-h-screen w-full bg-background">
-
       <Header />
       <main className="py-16 ">
         <HeroSection />
         <PortfolioMosaic />
         <CuratedWork />
+        <TechStackSection />
       </main>
     </div>
   );
